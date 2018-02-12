b081507d814f52105f16adaec6bf6106f60692bd
haric86 <hari.c57@wipro.com>
2018-02-12 21:34:23 +0530

    Global variables handling and login input parser[F

---
+++ globals/app_globals.py
@@ -0,0 +1,28 @@
+
+class AppElements(object):
+    SEARCH_BOX = 'mobile:id/el-ac'
+
+class ProductDetail(object):
+    PRODUCT_SEARCH = 'Google Nexus 5'
+    SEARCH_ELEMENT_ID = 'com.ebay.mobile:id/search_box'
+    ACTION_ELEMENT_ID = 'com.ebay.mobile:id/take_action'
+    ACCESS_ELEMENT_ID = 'Buy It Now'
+
+class Screenshot(object):
+    SS_CAPTURE_SETUP = 'setup.PNG'
+    SS_CAPTURE_TEARDOWN = 'teardown.PNG'
+    SS_CAPTURE_LOGIN = 'login.PNG'
+    SS_CAPTURE_SWIPE = 'swipe.PNG'
+    SS_CAPTURE_ORIENTATION = 'orientation.PNG'
+    SS_CAPTURE_PAYMENT = 'payment_methods.PNG'
+    SS_CAPTURE_PRODUCT_SELECT = 'product_select.PNG'
+
+class SendKeys(object):
+    ENTER = 'enter'
+
+class Payment(object):
+    PAYELEMENTID = 'proceedToPay'
+    MODE = 'com.ebay.mobile:id/mode'
+    NETBANKING = 'com.ebay.mobile:name/net_banking'
+    CARDS = 'com.ebay.mobile:name/cards'
+    WALLET = 'com.ebay.mobile:name/wallet'
